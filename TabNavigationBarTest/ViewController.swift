5414291ba6715ad6cf94719afdf5cce22b98bef0
Karlo Alejandro López Mayen MBP15 <karlo@qzbyte.com>
2019-10-05 16:49:17 -0500

    Initial Commit

---
+++ TabNavigationBarTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabNavigationBarTest
+//
+//  Created by Karlo A. López on 10/5/19.
+//  Copyright © 2019 Karlo A. López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
